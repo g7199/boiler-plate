e91969325ced6d7f699e0b59db2a05937f3eef19
g7199 <g7199@naver.com>
2021-03-01 03:28:57 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://g7199:bj825719@boilerplate.fqfg1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
